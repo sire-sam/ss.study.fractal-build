d7b587290b0d825fd0d94884a2fc9b3843238182
sire-sam <sire-sam@users.noreply.github.com>
2017-10-03 22:22:18 +0200

    add file (alert) and config (blockquote) variants implementation

--- src/components/blockquote.config.js
+++ src/components/blockquote.config.js
@@ -2,7 +2,16 @@ module.exports = {
   title: 'A simple blockquote component',
   status: 'wip',
   context: {
+    modifier: false,
     text: 'blockquote are the best!',
     citation: 'Fractal Docs',
   },
+  variants: [
+    {
+      name: 'fancy',
+      context: {
+        modifier: 'fancy',
+      },
+    },
+  ],
 };
