4acd00b2c5b203921aec754f9ad91191a06c6352
sire-sam <sire-sam@users.noreply.github.com>
2017-10-03 22:57:15 +0200

    add preview template with external css
    1. set static path in fractal.web config
    2. create css file in static.path, and hidden component for preview
       template. Link css using `path` nunjucks filter
    3. set default.preview template in fractal.components using `handle`

--- fractal.js
+++ fractal.js
@@ -3,9 +3,12 @@ const nunj = require('@frctl/nunjucks');
 
 fractal.set('project.title', 'FooCorp Component Library');
 
+fractal.web.set('static.path', `${__dirname}/src/public`);
+
 fractal.components.set('path', `${__dirname}/src/components`);
 fractal.components.engine(nunj);
 fractal.components.set('ext', '.nunj');
+fractal.components.set('default.preview', '@preview');
 
 fractal.docs.set('path', `${__dirname}/src/doc`);
 
