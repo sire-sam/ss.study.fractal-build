d5dd34971942f1bf3525918d508e1d78d46fc3a0
sire-sam <sire-sam@users.noreply.github.com>
2017-10-10 20:05:14 +0200

    Add component configuration example

--- src/components/button.config.js
+++ src/components/button.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   status: 'ready',
+  label: 'Boutton',
+  title: 'A simple button',
+  tags: ['CTA', 'Button'],
+  order: 1,
   context: {
     button: {
       text: 'Click Meee!',
